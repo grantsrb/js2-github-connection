89cfc6cf7b97264ae6f0fe39749df4939331c967
Satchel Grant <grantsrb@gmail.com>
2016-10-14 13:36:00 -0700

    increase page size in repo request to github

--- js/api-call.js
+++ js/api-call.js
@@ -3,14 +3,16 @@ var apiKey = require("./../.env").apiKey;
 function ApiCall() {
 }
 
-function repoInfo(url, displayFxn, clearFxn, userName) {
+function repoInfo(url, displayFxn, clearFxn, userName, img) {
   $.get(url).then(function(response) {
     clearFxn();
+    console.log(url);
+    console.log(JSON.stringify(response));
     for(var i = 0; i < response.length; i++) {
       var date = response[i].created_at;
       var projName = response[i].name;
       var description = response[i].description;
-      displayFxn(userName, projName, description, date);
+      displayFxn(userName, projName, description, date, img);
     }
   }).fail(function(response) {
     console.log(JSON.stringify(response));
@@ -26,6 +28,9 @@ ApiCall.prototype.userInfo = function(userName, displayFxn, clearFxn, invalidFxn
   }
   $.get(url).then(function(response) {
     url = response.repos_url;
+    url = url + "?page=1&per_page=100&";
+    console.log(url);
+    var img = response.avatar_url;
     if(urlKeyExists) {
       console.log("Added");
       url = url + '?access_token=' + apiKey;
@@ -33,7 +38,7 @@ ApiCall.prototype.userInfo = function(userName, displayFxn, clearFxn, invalidFxn
     var name = response.name;
     if (name == null)
       name = userName;
-    repoInfo(url, displayFxn, clearFxn, name);
+    repoInfo(url, displayFxn, clearFxn, name, img);
   }).fail(function(response) {
     invalidFxn(userName);
   });
--- js/github-interface.js
+++ js/github-interface.js
@@ -1,7 +1,9 @@
 var ApiCall = require("./../js/api-call.js").apiCallModule;
 
-var displayUserInfo = function(userName, projName, projDescription, date) {
+var displayUserInfo = function(userName, projName, projDescription, date, img) {
   $("#output-name").text(userName);
+  $("#user-image").attr("src", img);
+  $("#user-image").show();
   $("#pg1-repos").text("Page 1 repos:");
   $("#output").append("<li><strong>" + projName + "</strong></li>"+
     "<ul><li>Description: <em>" + projDescription + "</em></li>"+
@@ -13,6 +15,7 @@ var clearUserInfo = function() {
 }
 
 var invalidInput = function(userName) {
+  $("#user-image").hide();
   $("#pg1-repos").text("");
   $("#output").empty();
   $("#output-name").text("That username doesn't seem to exist :(");
