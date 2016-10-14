5ddeaced69a86a7a70bce388034541ecebe69356
Satchel Grant <grantsrb@gmail.com>
2016-10-14 09:15:08 -0700

    add clear fxn in output before displaying repo list

--- js/api-call.js
+++ js/api-call.js
@@ -3,8 +3,9 @@ var apiKey = require("./../.env").apiKey;
 function ApiCall() {
 }
 
-function repoInfo(url, displayFxn) {
+function repoInfo(url, displayFxn, clearFxn) {
   $.get(url).then(function(response) {
+    clearFxn();
     for(var i = 0; i < response.length; i++) {
       var projName = response[i].name;
       var description = response[i].description;
@@ -15,10 +16,10 @@ function repoInfo(url, displayFxn) {
   });
 }
 
-ApiCall.prototype.userInfo = function(userName, displayFxn) {
+ApiCall.prototype.userInfo = function(userName, displayFxn, clearFxn) {
   $.get('https://api.github.com/users/' + userName + '?access_token=' + apiKey).then(function(response) {
     var url = response.repos_url + '?access_token=' + apiKey;
-    repoInfo(url, displayFxn);
+    repoInfo(url, displayFxn, clearFxn);
   }).fail(function(response) {
     console.log(response);
   });
--- js/github-interface.js
+++ js/github-interface.js
@@ -4,11 +4,16 @@ var displayUserInfo = function(projName, projDescription) {
   $("#output").append("<li><strong>" + projName + "</strong></li><ul><li><em>" + projDescription + "</em></li></ul>");
 }
 
+var clearUserInfo = function() {
+  $("#output").empty();
+}
+
 $(function() {
   $("#user-form").submit(function(event){
     event.preventDefault();
     var userName = $("#user-name").val();
+    $("#output-name").text(userName + " page 1 repos:")
     var apiCall = new ApiCall();
-    apiCall.userInfo(userName, displayUserInfo);
+    apiCall.userInfo(userName, displayUserInfo, clearUserInfo);
   });
 });
